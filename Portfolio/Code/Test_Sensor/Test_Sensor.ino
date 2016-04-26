fb5a5850b93922940f219d4f47d7c5931cdd0975
John <john@imprecords.com>
2016-04-26 17:12:06 +0200

    Test sensor V1.1

--- Portfolio/Code/Test_Sensor/Test_Sensor.ino
+++ Portfolio/Code/Test_Sensor/Test_Sensor.ino
@@ -1,9 +1,13 @@
 #include <SensorX.h>
-
 SensorX SensorFront(2,3,4);
 SensorX SensorRight(4,5,4);
 SensorX SensorLeft(6,7,4);
+SensorX SensorBack(8,9,4);
 
+bool Voor;
+bool Rechts;
+bool Links;
+bool Achter;
 
 
 
@@ -14,14 +18,39 @@ void setup() {
 }
 
 void loop() {
-  if(SensorFront.Result()&&SensorRight.Result()==false&&SensorLeft.Result()==false){
-    Serial.println("Voor");
-  }
-  if(SensorRight.Result()&&SensorFront.Result()==false&&SensorLeft.Result()==false){
-    Serial.println("Rechts");
-  }
-  if(SensorLeft.Result()&&SensorRight.Result()==false&&SensorFront.Result()==false){
-    Serial.println("Links");
-  }
-
+  Sensor_Status();
+  Sensors_Result();
+  
 }
+void Sensor_Status(){
+  Voor=SensorFront.Result();
+  Rechts=SensorRight.Result();
+  Links=SensorLeft.Result();
+  Achter=SensorBack.Result(); 
+}
+
+void Sensors_Result(){
+  if(Voor&&Rechts==false&&Links==false&&Achter==false){
+    Serial.println("Allen Voor");
+  }
+  if(Voor&&Rechts&&Links==false&&Achter==false){
+    Serial.println("Voor en Links");
+  }
+  if(Voor&&Rechts==false&&Links&&Achter==false){
+    Serial.println("Voor en Links");
+  }
+  if(Voor==false&&Rechts==false&&Links&&Achter==false){
+    Serial.println("Allen Links");
+  }
+  if(Voor==false&&Rechts&&Links==false&&Achter==false){
+    Serial.println("Allen Rechts");
+  }
+  if(Voor==false&&Rechts==false&&Links==false&&Achter){
+    Serial.println("Allen Achter");
+  }
+  else{
+    Serial.println("");
+  }
+  
+}
+
