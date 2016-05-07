1ae9dadb1760e6ca375d8060bc31d52f13ab7111
GianniLenaerts <gianni.lenaerts@hotmail.com>
2016-05-07 16:19:45 +0200

    change led

--- Portfolio/RoboX_android_app/app/src/main/java/com/example/giann_000/robox_official_20/BluetoothConnection.java
+++ Portfolio/RoboX_android_app/app/src/main/java/com/example/giann_000/robox_official_20/BluetoothConnection.java
@@ -51,6 +51,8 @@ public class BluetoothConnection {
             findBT();
 
             openBT();
+            //bluetoothledje
+            sendData("b");
             return "Connected";
         }
         catch (Exception ex) {
